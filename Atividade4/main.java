3a74d875fafc3e64443407d412444b92cf2ebcfa
JoaoVictorSL2 <joaovictorlirasaraiva@gmail.com>
2025-09-04 18:31:09 -0300

    Add files via upload

---
+++ Atividade4/main.java
@@ -0,0 +1,13 @@
+package AtividadeLSP;
+
+public class Main {
+    public static void main(String[] args) {
+        Forma retangulo = new Retangulo(10, 5);
+        Forma quadrado = new Quadrado(7);
+        Forma circulo = new Circulo(4.5);
+
+        System.out.println("Área do retângulo: " + retangulo.calculaArea());
+        System.out.println("Área do quadrado: " + quadrado.calculaArea());
+        System.out.println("Área do círculo: " + circulo.calculaArea());
+    }
+}
